5252fbec19e1f3f855a08197601db4e218ad45b8
WillieMaddox <willie.maddox@gmail.com>
2025-02-04 00:55:47 -0600

    Refactor webpack.dev.js so it aligns mor closely with webpack.prod.js.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,46 +1,50 @@
-const path = require("path");
-const webpack = require('webpack');
 const Dotenv = require("dotenv-webpack");
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = merge([common, {
-    mode: 'development',
-    output: {
-        filename: '[name].bundle.js', // DEV
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+
+
+
+module.exports = merge([
+    common,
+    {
+        mode: 'development',
+        output: {
+            filename: '[name].bundle.js', // DEV
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+            ]
+        },
+        plugins: [
+            new Dotenv({
+                path: `./.env.development`
+            }),
+        ],
+        devtool: 'inline-source-map',
+        devServer: {
+            hot: true,
+            compress: true,
+            static: './dist',
+            proxy: {
+                '/api': 'http://localhost:3050'
             },
-        ]
-    },
-    plugins: [
-        new Dotenv({
-            path: `./.env.development`
-        }),
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        hot: true,
-        compress: true,
-        static: './dist',
-        proxy: {
-            '/api': 'http://localhost:3050'
+            port: 3050,
+            // writeToDisk: true,
+            devMiddleware: {
+                index: false,
+                // mimeTypes: { phtml: 'text/html' },
+                publicPath: './dist',
+                serverSideRender: true,
+                writeToDisk: true,
+            },
         },
-        port: 3050,
-        // writeToDisk: true,
-        devMiddleware: {
-            index: false,
-            // mimeTypes: { phtml: 'text/html' },
-            publicPath: './dist',
-            serverSideRender: true,
-            writeToDisk: true,
+        optimization: {
+            runtimeChunk: true,
         },
-    },
-    optimization: {
-        runtimeChunk: true,
-    },
-}]);
+    }
+]);
